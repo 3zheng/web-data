6ddc5bbe8634ab872aa654f81c2d1b6d2a8298f7
3zheng <914509007@qq.com>
2024-08-01 18:18:10 -0400

    新增异常捕捉

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"runtime"
 
 	tablemiddleware "github.com/3zheng/web-data/table_middleware"
 	_ "github.com/denisenkom/go-mssqldb"
@@ -24,7 +25,7 @@ type DBConfig struct {
 
 // IP   string `json:"ip"`
 type ServerConfig struct {
-	Name      string `json:"name"`
+	Path      string `json:"path"`
 	ForceIPv4 int    `json:"force ipv4"`
 	IP        string `json:"ip"`
 	Port      int    `json:"port"`
@@ -36,23 +37,56 @@ type Config struct {
 }
 
 func InitLog() {
-	logFile, err := os.OpenFile("./logfile", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	var filepath string
+	fmt.Println(runtime.GOOS)
+	if runtime.GOOS == "windows" {
+		filepath = "./logfile"
+	} else if runtime.GOOS == "linux" {
+		filepath = "/home/wuzhibin86/workspace/web-data/logfile"
+	} else {
+		fmt.Println("系统不明")
+		os.Exit(0)
+	}
+	logFile, err := os.OpenFile(filepath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
 		fmt.Println("open log file failed.")
 		return
 	}
 	log.SetOutput(logFile)
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Ltime)
+	log.Println("log file opened.")
+
+}
+
+func recovermain() {
+	if err := recover(); err != nil {
+		var buf [4096]byte
+		n := runtime.Stack(buf[:], false)
+		log.Fatalf("[panic] err: %v\nstack: %s\n", err, buf[:n])
+	}
 }
 
 func main() {
+	defer recovermain() //退出前打印异常
 	InitLog()
 	//读取配置文件
-	content, err := os.ReadFile("./config.json")
-	if err != nil {
-		log.Fatal("Error when opening file: ", err)
+	args := os.Args //main命令行参数
+	log.Println("main args = ", args)
+
+	var content []byte
+	var err error
+	//这时候如果不输入的命令行参数就会导致panic
+	if args[1] == "-config" {
+		log.Println("读取配置文件：", args[2])
+		content, err = os.ReadFile(args[2])
+		if err != nil {
+			log.Fatal("Error when opening file: ", err)
+		}
+	} else { //不带参数直接返回
+		return
 	}
 
+	//tablemiddleware.CreatePanic()
 	// Now let's unmarshall the data into `payload`
 	var config Config
 	err = json.Unmarshal(content, &config)
@@ -74,7 +108,12 @@ func main() {
 	//gin.SetMode(gin.ReleaseMode)
 
 	r := gin.Default()
-	r.LoadHTMLGlob("HTML/*") //加载HTML文件
+	htmlPath := "HTML/*"
+	if runtime.GOOS == "linux" {
+		htmlPath = config.Server.Path + htmlPath
+	}
+	log.Println(htmlPath)
+	r.LoadHTMLGlob(htmlPath) //加载HTML文件
 
 	r.GET("/KC", func(c *gin.Context) {
 		//var inventories [](*tablestruct.Inventory)
@@ -90,6 +129,7 @@ func main() {
 	//ln := net.Listener
 	if config.Server.ForceIPv4 == 1 {
 		// 强制使用IPv4
+		log.Println("强制使用IPv4")
 		server := &http.Server{Addr: addr, Handler: r}
 		ln, err := net.Listen("tcp4", addr)
 		if err != nil {
@@ -101,6 +141,7 @@ func main() {
 
 		server.Serve(tcpKeepAliveListener{ln.(*net.TCPListener)})
 	} else {
+		log.Println("http服务地址：", addr)
 		r.Run(addr)
 	}
 	//r.Run(addr)
--- table_middleware/table_strcut.go
+++ table_middleware/table_strcut.go
@@ -3,20 +3,36 @@ package tablestruct
 import (
 	"database/sql"
 	"log"
+	"os"
 )
 
 //这是数据库对应的表结构
 
 // 库存表对应dbo.View_KC
 type Inventory struct {
-	ProductID   string  `json:"ID"`          //产品类型
-	ProductName string  `json:"Name"`        //产品名字
-	ResidualNum float64 `json:"ResidualNum"` //库存数量
+	//产品型号,产品名称,产品描述,产品类型名称,主类型名称,产品单位名称,城市名称,仓库名称,库存数量,库存成本,最近30天销售数量,消纳时间
+	ProductID          string  `json:"ID"`                //产品型号
+	ProductName        string  `json:"Name"`              //产品名称
+	ProductDescription string  `json:"Descrption"`        //产品描述
+	ProductSubclass    string  `json:"Subclass"`          //产品类型名称,二级分类
+	ProductSuperClass  string  `json:"ProductSuperClass"` //主类型名称,一级分类
+	ProductUnitName    string  `json:"ProductUnitName"`   //产品单位名称
+	CityName           string  `json:"CityName"`          //城市名称
+	ResidualNum        float64 `json:"ResidualNum"`       //库存数量
+}
+
+func CreatePanic() {
+	if os.Args[4] == "" {
+		log.Println("arg 4:", os.Args[4])
+	}
 }
 
 func GetInventory(db *sql.DB) [](*Inventory) {
 	//编写查询语句
-	stmt, err := db.Prepare(`select 产品型号, 产品名称, 库存数量 from dbo.View_KC`)
+	//select 产品型号,产品名称,产品描述,产品类型名称,主类型名称,产品单位名称,城市名称,仓库名称,库存数量,库存成本,最近30天销售数量,消纳时间 from dbo.View_KC
+	stmt, err := db.Prepare(`select 产品型号,产品名称,产品描述,产品类型名称,主类型名称,产品单位名称,
+		城市名称,仓库名称,库存数量,库存成本,最近30天销售数量,消纳时间 
+		from dbo.View_KC`)
 	if err != nil {
 		log.Fatal("Prepare failed:", err.Error())
 	}
