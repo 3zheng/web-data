728636449f40a72498ff53ea6e8f7d324fdcec6d
3zheng <914509007@qq.com>
2024-09-05 18:53:05 -0400

    信号

--- signal.go
+++ signal.go
@@ -15,7 +15,7 @@ func CatchSighup() {
 	// 创建一个通道用于接收信号
 	signalChan := make(chan os.Signal, 1)
 
-	// 捕捉 SIGHUP 信号
+	// 捕捉 SIGHUP 信号 只有Linux有SIGHUP信号，windows没有
 	signal.Notify(signalChan, syscall.SIGHUP)
 
 	// 启动一个协程来监听信号
